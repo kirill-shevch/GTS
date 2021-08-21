6fd32d655db2106b1059a691d1eb43fdfbf74053
Kirill Shevchenko <kirillwowlol@yandex.ru>
2021-08-21 22:32:49 +0200

    Fix client coordinates

--- Server/GtsServer/GtsServer/Models/ServerPlayer.cs
+++ Server/GtsServer/GtsServer/Models/ServerPlayer.cs
@@ -3,8 +3,8 @@
     public class ServerPlayer
     {
         public string Name { get; set; }
-        public float X { get; set; } = 20;
-        public float Z { get; set; } = 20;
+        public float X { get; set; } = -10;
+        public float Z { get; set; } = -5;
         public string ConnectionId { get; set; }
         public Direction Direction { get; set; } = Direction.Top;
         public int Health { get; set; } = 5;
