cf1983b8449b002693b941cdf56b70a13a9a5d68
Kirill Shevchenko <kirillwowlol@yandex.ru>
2021-08-21 15:04:47 +0200

    Add separate script for user interface

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -8,7 +8,7 @@ using UnityEngine.UI;
 
 public class Main : MonoBehaviour
 {
-    private GameObject player;
+    
 
     private float movementSpeed = 2;
     private float synchronizationTime = 1.0f;
@@ -18,25 +18,6 @@ public class Main : MonoBehaviour
     void Start()
     {
         Screen.SetResolution(640, 480, false);
-        var loginButtonObject = GameObject.Find("OkButton");
-        loginButtonObject.GetComponentInChildren<Text>().text = "Ok";
-        var loginButton = loginButtonObject.GetComponent<Button>();
-        var nameFieldObject = GameObject.Find("NameField");
-        var nameField = nameFieldObject.GetComponent<InputField>();
-        var errorButtonObject = GameObject.Find("ErrorButton");
-        errorButtonObject.GetComponentInChildren<Text>().text = "Ok";
-        var errorButton = errorButtonObject.GetComponent<Button>();
-        var errorText = GameObject.Find("ErrorText").GetComponent<Text>();
-        var healthText = GameObject.Find("Health").GetComponent<Text>();
-        var userNameText = GameObject.Find("UserName").GetComponent<Text>();
-        var invulnerableStatus = GameObject.Find("InvulnerableStatus").GetComponent<Text>();
-        loginButton.onClick.AddListener(OnLoginClick);
-        errorButton.onClick.AddListener(OnErrorClick);
-        errorText.enabled = false;
-        healthText.enabled = false;
-        userNameText.enabled = false;
-        invulnerableStatus.enabled = false;
-        errorButtonObject.SetActive(false);
     }
 
     void Update()
@@ -59,7 +40,7 @@ public class Main : MonoBehaviour
             }
         }
         uidsToDelete.ForEach(x => SceneObjects.Projectiles.Remove(x));
-        if (player != null)
+        if (SceneObjects.Player != null)
         {
             synchronizationTime -= Time.deltaTime;
 
@@ -90,14 +71,14 @@ public class Main : MonoBehaviour
                 var horizontalInput = Input.GetAxis("Horizontal");
                 if (horizontalInput > 0)
                 {
-                    var targetPosition = player.transform.position + Vector3.right * movementSpeed;
-                    player.transform.position = Vector3.MoveTowards(player.transform.position, targetPosition, step);
+                    var targetPosition = SceneObjects.Player.transform.position + Vector3.right * movementSpeed;
+                    SceneObjects.Player.transform.position = Vector3.MoveTowards(SceneObjects.Player.transform.position, targetPosition, step);
                     SceneObjects.UserModel.Direction = Direction.Right;
                 }
                 else
                 {
-                    var targetPosition = player.transform.position + Vector3.left * movementSpeed;
-                    player.transform.position = Vector3.MoveTowards(player.transform.position, targetPosition, step);
+                    var targetPosition = SceneObjects.Player.transform.position + Vector3.left * movementSpeed;
+                    SceneObjects.Player.transform.position = Vector3.MoveTowards(SceneObjects.Player.transform.position, targetPosition, step);
                     SceneObjects.UserModel.Direction = Direction.Left;
                 }
             }
@@ -107,14 +88,14 @@ public class Main : MonoBehaviour
                 var varticalInput = Input.GetAxis("Vertical");
                 if (varticalInput > 0)
                 {
-                    var targetPosition = player.transform.position + Vector3.forward * movementSpeed;
-                    player.transform.position = Vector3.MoveTowards(player.transform.position, targetPosition, step);
+                    var targetPosition = SceneObjects.Player.transform.position + Vector3.forward * movementSpeed;
+                    SceneObjects.Player.transform.position = Vector3.MoveTowards(SceneObjects.Player.transform.position, targetPosition, step);
                     SceneObjects.UserModel.Direction = Direction.Top;
                 }
                 else
                 {
-                    var targetPosition = player.transform.position + Vector3.back * movementSpeed;
-                    player.transform.position = Vector3.MoveTowards(player.transform.position, targetPosition, step);
+                    var targetPosition = SceneObjects.Player.transform.position + Vector3.back * movementSpeed;
+                    SceneObjects.Player.transform.position = Vector3.MoveTowards(SceneObjects.Player.transform.position, targetPosition, step);
                     SceneObjects.UserModel.Direction = Direction.Bot;
                 }
             }
@@ -134,62 +115,13 @@ public class Main : MonoBehaviour
     private void TimerEnded()
     {
         synchronizationTime = 0.05f;
-        SceneObjects.UserModel.X = player.transform.position.x;
-        SceneObjects.UserModel.Z = player.transform.position.z;
+        SceneObjects.UserModel.X = SceneObjects.Player.transform.position.x;
+        SceneObjects.UserModel.Z = SceneObjects.Player.transform.position.z;
         ServerHub.Synchronize(SceneObjects.UserModel.ConvertToServerPlayer());
     }
 
     void OnDestroy()
     {
         ServerHub.CloseConnection();
-    }
-
-    void OnLoginClick()
-    {
-        userName = GameObject.Find("NameField").GetComponent<InputField>().text;
-        if (string.IsNullOrWhiteSpace(userName))
-        {
-            GameObject.Find("OkButton").SetActive(false);
-            GameObject.Find("NameField").SetActive(false);
-            GameObject.Find("ErrorText").GetComponent<Text>().text = "User name should not be empty!";
-            GameObject.Find("ErrorText").GetComponent<Text>().enabled = true;
-            GameObject.Find("ErrorButton").SetActive(true);
-        }
-        else
-        {
-            player = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            player.transform.position = new Vector3(20, 1, 20);
-            player.name = userName;
-            var rigidbody = player.AddComponent<Rigidbody>();
-            rigidbody.constraints = RigidbodyConstraints.FreezePositionY |
-                RigidbodyConstraints.FreezeRotationX |
-                RigidbodyConstraints.FreezeRotationY |
-                RigidbodyConstraints.FreezeRotationZ;
-            var playerRenderer = player.GetComponent<Renderer>();
-            playerRenderer.material.SetColor("_Color", UnityEngine.Random.ColorHSV());
-            SceneObjects.UserModel.Name = userName;
-            SceneObjects.UserModel.ConnectionId = ServerHub.Connection.ConnectionId;
-
-            GameObject.Find("OkButton").SetActive(false);
-            GameObject.Find("NameField").SetActive(false);
-            ServerHub.AddUserName(userName);
-            var userNameText = GameObject.Find("UserName").GetComponent<Text>();
-            userNameText.text = userName;
-            userNameText.enabled = true;
-            var healthText = GameObject.Find("Health").GetComponent<Text>();
-            healthText.text = SceneObjects.UserModel.Health.ToString();
-            healthText.enabled = true;
-            var invulnerableStatus = GameObject.Find("InvulnerableStatus").GetComponent<Text>();
-            invulnerableStatus.text = SceneObjects.UserModel.IsInvulnerable ? "Invulnerable" : string.Empty;
-            invulnerableStatus.enabled = true;
-        }
-    }
-
-    void OnErrorClick()
-    {
-        GameObject.Find("OkButton").SetActive(true);
-        GameObject.Find("NameField").SetActive(true);
-        GameObject.Find("ErrorText").GetComponent<Text>().enabled = false;
-        GameObject.Find("ErrorButton").SetActive(false);
     }
 }
---
+++ Assets/UserInterfaceBehavior.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets
+{
+    public static class UserInterfaceBehavior
+    {
+        public static void Initialize()
+        {
+            var loginButtonObject = GameObject.Find("OkButton");
+            loginButtonObject.GetComponentInChildren<Text>().text = "Ok";
+            var loginButton = loginButtonObject.GetComponent<Button>();
+            var nameFieldObject = GameObject.Find("NameField");
+            var nameField = nameFieldObject.GetComponent<InputField>();
+            var errorButtonObject = GameObject.Find("ErrorButton");
+            errorButtonObject.GetComponentInChildren<Text>().text = "Ok";
+            var errorButton = errorButtonObject.GetComponent<Button>();
+            var errorText = GameObject.Find("ErrorText").GetComponent<Text>();
+            var healthText = GameObject.Find("Health").GetComponent<Text>();
+            var userNameText = GameObject.Find("UserName").GetComponent<Text>();
+            var invulnerableStatus = GameObject.Find("InvulnerableStatus").GetComponent<Text>();
+            loginButton.onClick.AddListener(OnLoginClick);
+            errorButton.onClick.AddListener(OnErrorClick);
+            errorText.enabled = false;
+            healthText.enabled = false;
+            userNameText.enabled = false;
+            invulnerableStatus.enabled = false;
+            errorButtonObject.SetActive(false);
+        }
+
+        private static void OnLoginClick()
+        {
+            var userName = GameObject.Find("NameField").GetComponent<InputField>().text;
+            if (string.IsNullOrWhiteSpace(userName))
+            {
+                GameObject.Find("OkButton").SetActive(false);
+                GameObject.Find("NameField").SetActive(false);
+                GameObject.Find("ErrorText").GetComponent<Text>().text = "User name should not be empty!";
+                GameObject.Find("ErrorText").GetComponent<Text>().enabled = true;
+                GameObject.Find("ErrorButton").SetActive(true);
+            }
+            else
+            {
+                SceneObjects.Player = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                SceneObjects.Player.transform.position = new Vector3(20, 1, 20);
+                SceneObjects.Player.name = userName;
+                var rigidbody = SceneObjects.Player.AddComponent<Rigidbody>();
+                rigidbody.constraints = RigidbodyConstraints.FreezePositionY |
+                    RigidbodyConstraints.FreezeRotationX |
+                    RigidbodyConstraints.FreezeRotationY |
+                    RigidbodyConstraints.FreezeRotationZ;
+                var playerRenderer = SceneObjects.Player.GetComponent<Renderer>();
+                playerRenderer.material.SetColor("_Color", UnityEngine.Random.ColorHSV());
+                SceneObjects.UserModel.Name = userName;
+                SceneObjects.UserModel.ConnectionId = ServerHub.Connection.ConnectionId;
+
+                GameObject.Find("OkButton").SetActive(false);
+                GameObject.Find("NameField").SetActive(false);
+                ServerHub.AddUserName(userName);
+                var userNameText = GameObject.Find("UserName").GetComponent<Text>();
+                userNameText.text = userName;
+                userNameText.enabled = true;
+                var healthText = GameObject.Find("Health").GetComponent<Text>();
+                healthText.text = SceneObjects.UserModel.Health.ToString();
+                healthText.enabled = true;
+                var invulnerableStatus = GameObject.Find("InvulnerableStatus").GetComponent<Text>();
+                invulnerableStatus.text = SceneObjects.UserModel.IsInvulnerable ? "Invulnerable" : string.Empty;
+                invulnerableStatus.enabled = true;
+            }
+        }
+
+        private static void OnErrorClick()
+        {
+            GameObject.Find("OkButton").SetActive(true);
+            GameObject.Find("NameField").SetActive(true);
+            GameObject.Find("ErrorText").GetComponent<Text>().enabled = false;
+            GameObject.Find("ErrorButton").SetActive(false);
+        }
+    }
+}
