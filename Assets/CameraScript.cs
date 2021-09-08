63ad38ce50b9e3aa205769ae517785b0e7157b90
Kirill Shevchenko <kirillwowlol@yandex.ru>
2021-09-08 12:22:08 +0200

    Add camera bind to player model

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,18 @@
+﻿using Assets;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    private float movementSpeed = 2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SceneObjects.Player != null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position,
+                new Vector3(SceneObjects.Player.transform.position.x, transform.position.y, SceneObjects.Player.transform.position.z-3.5f),
+                movementSpeed);
+        }
+    }
+}
